cfdb7626cdfacc24b55e890ee7746712f594a7d6
realsushi <79677326+realshizi@users.noreply.github.com>
2021-11-03 19:10:23 -0400

    magicked
    smol

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-UnitTypes.oxynoe.weapons.each(w=>w.bullet=new LiquidBulletType(Liquids.cryofluid));UnitTypes.oxynoe.weapons.each(w=>w.bullet.damage=30);UnitTypes.oxynoe.weapons.each(w=>w.bullet.speed=1.8)
+UnitTypes.oxynoe.weapons.each(w=>w.bullet=new LiquidBulletType(Liquids.cryofluid));UnitTypes.oxynoe.weapons.each(w=>{w.bullet.damage=30;w.bullet.speed=1.8});
